dcc7c4fcaa56d7fa869a86e2a612b8882f222432
shyoup <46154001+shyoup@users.noreply.github.com>
2019-01-07 00:02:49 +0900

    Update main.cpp

--- 1240.단순2진암호코드/main.cpp
+++ 1240.단순2진암호코드/main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int num[10][7]{
-	{0,0,0,1,1,0,1},
-	{0,0,1,1,0,0,1},
-	{0,0,1,0,0,1,1},
-	{0,1,1,1,1,0,1},
-	{0,1,0,0,0,1,1},
-	{0,1,1,0,0,0,1},
-	{0,1,0,1,1,1,1},
-	{0,1,1,1,0,1,1},
-	{0,1,1,0,1,1,1},
-	{0,0,0,1,0,1,1}
+{ 0,0,0,1,1,0,1 },
+{ 0,0,1,1,0,0,1 },
+{ 0,0,1,0,0,1,1 },
+{ 0,1,1,1,1,0,1 },
+{ 0,1,0,0,0,1,1 },
+{ 0,1,1,0,0,0,1 },
+{ 0,1,0,1,1,1,1 },
+{ 0,1,1,1,0,1,1 },
+{ 0,1,1,0,1,1,1 },
+{ 0,0,0,1,0,1,1 }
 };
 
 const int index = 6;
@@ -23,7 +23,7 @@ int main() {
 	int C;
 	cin >> C;
 	for (int tc = 0; tc < C; tc++) {
-		int pass[8] = { 0. };
+		int pass[8] = { 0, };
 		int N, M, x;
 		int ii;
 		int result = 0;
@@ -39,22 +39,12 @@ int main() {
 			for (int b = 0; b < M; b++) {
 				scanf_s("%1d", &x);
 				password[a][b] = x;
+				if (x == 1)
+					ii = a;
 			}
 		} // 데이터 넣기
-		for (int a = 0; a < N; a++) {
-			for (int c = 0; c < M; c++) {
-				if (password[a][c] != 0) {
-					ii = a;
-					ret = true;
-					break;
-				}
-			}
-			if (ret == true)
-				break;
-		}
-
 		for (int i = 0; i < M - 6; i++) {
-			if (password[ii][index + i] == 1 && password[ii][index+i-6] ==0 && password[ii][index+i+1] !=1) {
+			if (password[ii][index + i] == 1 && password[ii][index + i - 6] == 0 && password[ii][index + i + 1] != 1) {
 				for (int numindex = 0; numindex < 10; numindex++) {
 					for (int aa = 0; aa < 7; ++aa) {
 						if (password[ii][index + i - 6 + aa] == num[numindex][aa]) {
@@ -78,7 +68,7 @@ int main() {
 			}
 		}
 
-		cout << "#" << tc+1 << " " << result << endl;
+		cout << "#" << tc + 1 << " " << result << endl;
 		for (int i = 0; i < N; i++) {
 			delete[] password[i];
 		}
@@ -87,3 +77,4 @@ int main() {
 	}
 	return 0;
 }
+// 사실 for문이 너무 많아서 timecomplexity가 굉장히 높을 것이다. 다음에는 좋은 코드를 짜도록 해보자
