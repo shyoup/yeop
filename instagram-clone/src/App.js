b6baa166ee1f5af7c0a023155e027f498326dcde
sanghyeop_jeon <sanghyeop_jeon@tmax.co.kr>
2021-08-10 23:31:26 +0900

    [feat] header 작업

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {/* Header */}
+      {/* Post */}
+      {/* Post */}
     </div>
   );
 }
