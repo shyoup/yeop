232d74bc679e6153f25f10178896e0e676b2be34
shyoup <46154001+shyoup@users.noreply.github.com>
2021-08-14 21:30:16 +0900

    [feat] modal 추가 임시 코드

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post'
 import { db } from './firebase'
+import { Modal } from '@material-ui/core';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     // this is where the code runs
@@ -19,6 +21,14 @@ function App() {
 
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am modal</h2>
+        </div>  
+      </Modal>
       <div className="app__header">
         <img
           className="app__headerImage"
