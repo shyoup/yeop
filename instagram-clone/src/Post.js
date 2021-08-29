0f09447b63fb7ba94ed9b14451b6d41c3f7709ca
sanghyeop_jeon <wjstkdguq92@naver.com>
2021-08-29 14:09:23 +0900

    [chore] css 변경

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -43,7 +43,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
                     alt="hyeop_92"
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/201111416_208072207837303_993554199430242986_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=qy00gAkB3QsAX_eUqYY&edm=AP_V10EBAAAA&ccb=7-4&oh=e6e6b21cd528e189ab919e86a150a31e&oe=611B6432&_nc_sid=4f375e"
                 ></Avatar>
-                <h3>{username}</h3>
+                {username}
             </div>
             <img className="post__image" src={imageUrl}/>
             <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
@@ -61,7 +61,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
                     <input
                         className="post__input"
                         type="text"
-                        placeholder="Add a comment..."
+                        placeholder="댓글 달기..."
                         value={comment}
                         onChange={(e) => setComment(e.target.value)}
                     />
@@ -71,7 +71,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
                         type="submit"
                         onClick={postComment}
                     >
-                        Post
+                        게시
                     </button>
                 </form>
             )}
