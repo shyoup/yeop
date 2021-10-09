bfe2497e2b72900f315018b811801632ab3689c3
sanghyeop_jeon <wjstkdguq92@naver.com>
2021-10-09 23:53:49 +0900

    [feat] 프로그래머스 -올바른 괄호

---
+++ CPP Algorithm/올바른괄호/main.cpp
@@ -0,0 +1,18 @@
+function solution(s){
+    var answer = true;
+    let stack = [];
+    
+    for(let i=0; i<s.length; i++) {
+        if(s[i] === "(") {
+            stack.push(s[i]);
+        } else {
+            if(stack.length === 0)
+                return false;
+            else
+                stack.pop();
+        }
+    }
+    if(stack.length > 0)
+        return false;    
+    return answer;
+}
