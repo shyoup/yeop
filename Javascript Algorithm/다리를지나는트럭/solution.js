0d8ff11fcc4bc267dd80d40e1aa7dd16e97ded33
sanghyeop_jeon <wjstkdguq92@naver.com>
2021-10-14 20:41:42 +0900

    [feat] 프로그래머스 - 다리를 지나는 트럭 수정

--- Javascript Algorithm/다리를지나는트럭/solution.js
+++ Javascript Algorithm/다리를지나는트럭/solution.js
@@ -22,30 +22,61 @@ return ++answer;
 }
  */  // 시간초과 코드
 
+// function solution(bridge_length, weight, truck_weights) {
+//     var answer = 0;
+//     let queue = [];
+//     for(let i=truck_weights.length; i>0; i--) {
+//         queue.push(truck_weights[i-1]);
+//     }
+//     let bridge = [0];
+//     let cur_weight = -1;
+//     while(queue.length > 0 || cur_weight !== 0) {
+//         answer++;
+//         let top = queue[queue.length - 1] ? queue[queue.length - 1] : 0;
+//         if(cur_weight + top > weight) { // 다리에 더이상 올라갈 수 없을 때
+//             bridge.unshift(0);
+//             if(bridge[bridge.length -1] > 0)
+//                 cur_weight -= bridge[bridge.length -1];
+//             bridge.pop();
+//         } else {    // 다리 트럭이 더 지나갈 수 있을 때
+//             bridge.unshift(top);
+//             if(cur_weight === -1)
+//                 cur_weight = top;
+//             else
+//                 cur_weight += top;
+//             queue.pop();
+//         }
+//     }    
+//     return answer;
+// }
+
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    let queue = [];
-    for(let i=truck_weights.length; i>0; i--) {
-        queue.push(truck_weights[i-1]);
-    }
-    let bridge = [0];
-    let cur_weight = -1;
-    while(queue.length > 0 || cur_weight !== 0) {
-        answer++;
-        let top = queue[queue.length - 1] ? queue[queue.length - 1] : 0;
-        if(cur_weight + top > weight) { // 다리에 더이상 올라갈 수 없을 때
-            bridge.unshift(0);
-            if(bridge[bridge.length -1] > 0)
-                cur_weight -= bridge[bridge.length -1];
-            bridge.pop();
-        } else {    // 다리 트럭이 더 지나갈 수 있을 때
-            bridge.unshift(top);
-            if(cur_weight === -1)
-                cur_weight = top;
-            else
-                cur_weight += top;
-            queue.pop();
+    let cur_weight = 0;
+    let answer = 0;
+    let onBridge = [];
+    let rest = [];
+    while(1) {
+        let _size = rest.length;
+        for(let i =0; i<_size; i++) {
+            let restLength = rest[0];
+            rest.shift();
+            if(restLength <= 1) {
+                cur_weight -= onBridge[0];
+                onBridge.shift();
+                continue;
+            }
+            rest.push(restLength - 1);
         }
-    }    
+        
+        if(truck_weights.length > 0 && truck_weights[0] + cur_weight <= weight) {
+            cur_weight += truck_weights[0];
+            onBridge.push(truck_weights[0]);
+            rest.push(bridge_length);
+            truck_weights.shift();
+        }
+        answer++;
+        if(cur_weight == 0 && truck_weights.length == 0)
+            break;
+    }
     return answer;
 }
