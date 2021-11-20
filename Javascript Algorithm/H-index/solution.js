8081349e0e3c6da22f05f82d0e114ac30755d4e5
sanghyeop_jeon <wjstkdguq92@naver.com>
2021-11-20 23:31:04 +0900

    [feat] 프로그래머스 - H-index

--- Javascript Algorithm/H-index/solution.js
+++ Javascript Algorithm/H-index/solution.js
@@ -2,10 +2,9 @@ function solution(citations) {
     let sortedArr = citations.sort((a,b)=> b-a);
     let idx = 0;
     for(let i = sortedArr[0]; i>=0; i--) {
-      if(i === sortedArr[idx]) {
-        idx++;
-      }
-      console.log(idx);
-      if(sortedArr[idx-1] <= idx) return sortedArr[idx-1];
+      if(i <= sortedArr[idx]) idx++;     // == 이 되어버리면 중복 체크를 못함
+      if(i <= idx) return i;
     }
   }
+  console.log(solution([47, 42, 32, 28, 24, 22, 17, 15, 10, 10, 8]));   // 10
+  console.log(solution([3, 0, 6, 1, 5]));
