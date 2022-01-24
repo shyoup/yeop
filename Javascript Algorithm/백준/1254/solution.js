65db743a882fa2677debd1a0b50e669d695901d5
sanghyeop_jeon <wjstkdguq92@naver.com>
2022-01-24 22:02:59 +0900

    [feat] 백준 1254

---
+++ Javascript Algorithm/백준/1254/solution.js
@@ -0,0 +1,21 @@
+function solution(plain) {
+  function isPalindrome(str) {
+      let half = parseInt(str.length / 2);
+      for (let i=0; i<half; i++) {
+          if(str[i] != str[str.length-1-i]) return false; 
+      }
+      return true;
+  }
+
+  let answer = plain.length * 2 - 1;
+
+  for (let i=0; i<plain.length; i++) {
+      let tmp = plain.substr(i, plain.length);
+      if(isPalindrome(tmp)) {
+          answer =  plain.length + i;
+          break;
+      }
+  }
+
+  return answer;
+}
