a79e7cb5f47e7c5cbdc706c7dd82c6a5571e2b76
sanghyeop_jeon <wjstkdguq92@naver.com>
2022-03-24 23:30:49 +0900

    [feat] 프로그래머스 - 체육복

---
+++ CPP Algorithm/체육복/solution.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    vector<int> cloths(n, 1);
+    for(int i=0; i<lost.size(); i++) {
+       int index = lost[i] - 1;
+       cloths[index] -= 1;
+    }
+
+    for(int j=0; j<reserve.size(); j++) {
+       int index = reserve[j] - 1;
+       cloths[index] += 1;
+    }
+
+    int nocloth = 0;
+    for(int k=0; k<cloths.size(); k++) {
+        if(cloths[k] == 0) {
+            if(k-1 > -1 && cloths[k-1] > 1) {
+                if(cloths[k-1] > 1) cloths[k-1] -= 1;
+            } else if(k+1 < cloths.size() && cloths[k+1] > 1) {
+                cloths[k+1] -= 1;
+            } else {
+                ++nocloth;
+            }
+        }
+    }
+    return n-nocloth;
+}
