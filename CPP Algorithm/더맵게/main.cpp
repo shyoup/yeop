79b2eaa858adf884ceee1c9cddda1eb61ff42a2d
sanghyeop_jeon <wjstkdguq92@naver.com>
2022-04-06 22:24:11 +0900

    [chore] 프로그래머스-더맵게 수정:

--- CPP Algorithm/더맵게/main.cpp
+++ CPP Algorithm/더맵게/main.cpp
@@ -7,7 +7,6 @@ priority_queue<int, vector<int>, greater<int>> cp;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    bool result = false;
     for(int i = 0; i<scoville.size(); i++) {
         cp.push(scoville[i]);
     }   
