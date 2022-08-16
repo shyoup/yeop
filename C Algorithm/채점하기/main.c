127f0bc878fdbfed4fcf86976b8355462e8fd95f
sanghyeop_jeon <wjstkdguq92@naver.com>
2022-08-16 20:46:22 +0900

    [chore] 주석 추가

--- C Algorithm/채점하기/main.c
+++ C Algorithm/채점하기/main.c
@@ -5,7 +5,7 @@ int main() {
 	int cur = 0;
 	int sum = 0;
 	scanf("%s",input);
-	for(int i=0; i<strlen(input); i++) {
+	for(int i=0; i<strlen(input); i++) {   // c에서 문자열 길이는 strlen으로 체크
 		cur++;
 		if(input[i] == 'x') {
 			cur = 0;
