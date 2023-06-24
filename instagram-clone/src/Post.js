6dd444e1694e10fa846a84933110604a4abecbc2
sanghyeop-jeon <wjstkdguq92@kakao.com>
2023-06-24 23:22:51 +0900

    h로만 나오던 문제 수정

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -90,7 +90,7 @@ function Post({ postId, user, username, caption, imageUrl, timeStr }) {
             <div className="post__header">
                 <Avatar
                     className="post__avatar"
-                    alt="hyeop_92"
+                    alt={username}
                     src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/201111416_208072207837303_993554199430242986_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=qy00gAkB3QsAX_eUqYY&edm=AP_V10EBAAAA&ccb=7-4&oh=e6e6b21cd528e189ab919e86a150a31e&oe=611B6432&_nc_sid=4f375e"
                 ></Avatar>
                 {username}
